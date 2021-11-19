86c768dd26c84534163086208ae285af920e573f
Harold_Root <harold_root@163.com>
2021-11-19 08:01:37 -0800

    test

--- src/observer/sql/executor/execute_stage.cpp
+++ src/observer/sql/executor/execute_stage.cpp
@@ -582,9 +582,8 @@ RC ExecuteStage::do_select(const char *db, Query *sql, SessionEvent *session_eve
         break;
       }
       if(selects.group_attr_num!=0){
-        ss << "FAILURE\n";
-        return RC::ABORT;
         GroupTupleSet group_set(&res_set.back(),_select,print_order);
+        return RC::ABORT;
         rc = group_set.set_by_field(_select,true);
         if(rc!=RC::SUCCESS){
           ss << "FAILURE\n";
